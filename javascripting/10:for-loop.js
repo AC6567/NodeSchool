35a4a8a0a45e65c4af3d8776a3e9c850fe95b6c5
Perrin Clark <blackguycoding@gmail.com>
2016-04-22 17:29:35 +0300

    submit for loop solution

---
+++ javascripting/10:for-loop.js
@@ -0,0 +1,13 @@
+/* Challenge #10: for loop
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.nodeschool.io */
+
+var total = 0;
+var limit = 10;
+
+for (var i = 0; i < limit; i++) {
+  total += i;
+}
+
+console.log(total)
--- javascripting/for-loop.js
+++
@@ -1,4 +0,0 @@
-/* Challenge #10: for loop
-Code by Perrin Clark
-BlackGuyCoding@gmail.com
-https://www.nodeschool.io */
