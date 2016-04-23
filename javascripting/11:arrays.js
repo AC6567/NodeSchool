834e0641547acab536bc0e08eace319f6858093a
Perrin Clark <blackguycoding@gmail.com>
2016-04-23 12:54:23 +0300

    added arrays solution

---
+++ javascripting/11:arrays.js
@@ -0,0 +1,7 @@
+/* Challenge #11: arrays
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.nodeschool.io */
+
+var pizzaToppings = ['tomato sauce', 'cheese', 'pepperoni'];
+console.log(pizzaToppings);
