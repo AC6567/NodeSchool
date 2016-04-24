6ee33072902b336fbdf443303565842f81ca81e2
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 18:15:34 +0300

    create new solution

---
+++ Core/git-it/7:branches.js
@@ -0,0 +1,7 @@
+/* Challenge #7 branches aren't just for birds
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.nodeschool.io */
+
+
+//this challenge requires you to create a new branch on your fork for your contribution.//
