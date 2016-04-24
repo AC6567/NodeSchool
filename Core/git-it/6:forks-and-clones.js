6b715029f749870933962bf8863b751b8613fd86
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 18:15:31 +0300

    create new solution

---
+++ Core/git-it/6:forks-and-clones.js
@@ -0,0 +1,7 @@
+/* Challenge #6 forks and clones
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.nodeschool.io */
+
+
+//this challenge requires you to fork a project from GitHub.com and clone it locally//
