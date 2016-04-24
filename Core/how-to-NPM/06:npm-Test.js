20752afa3ccf6e30599edc0617b94bfa273309fc
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 21:20:42 +0300

    create new solution

---
+++ Core/how-to-NPM/06:npm-Test.js
@@ -0,0 +1,7 @@
+/* Challenge #6 npm Test
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.nodeschool.io */
+
+
+//this challenge requires you to create a test file for your npm package //
