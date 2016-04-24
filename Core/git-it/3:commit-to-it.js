cf7a20522177a307f92f7a366f396a805ba7d5af
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 18:15:22 +0300

    create new solution

---
+++ Core/git-it/3:commit-to-it.js
@@ -0,0 +1,7 @@
+/* Challenge #3 commit to it
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.nodeschool.io */
+
+
+//this challenge requires you commit a new change to your repository//
