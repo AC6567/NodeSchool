63ea065b17415785adc27be6fab7252aaa1f2ec0
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 17:21:43 +0300

    create solution for challenge

---
+++ Core/git-it/2:commit-to-it.js
@@ -0,0 +1,7 @@
+/* Challenge #1 git it:
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.nodeschool.io */
+
+
+//this challenge requires you create a new (or have an existing) repository on your local machine//
