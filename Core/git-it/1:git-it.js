2e0e3c23b0159870d08b81eceaf85423ac9ab4ee
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 17:21:40 +0300

    create solution for challenge

---
+++ Core/git-it/1:git-it.js
@@ -0,0 +1,7 @@
+/* Challenge #1 git it:
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.nodeschool.io */
+
+
+//this challenge requires you to download and setup the Git desktop application from https://desktop.github.com/ //
