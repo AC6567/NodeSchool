7bf09759e8fdd428c20e1cf78488698124d5a65c
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 18:15:28 +0300

    create new solution

---
+++ Core/git-it/5:remote-control.js
@@ -0,0 +1,8 @@
+/* Challenge #5 remote control
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.nodeschool.io */
+
+
+//this challenge requires you to connect your local and remote repositories and push changes.
+//
