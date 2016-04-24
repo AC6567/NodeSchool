01eb867a1ec2769d70efa35b8f18fb11cbb5b3c6
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 21:20:16 +0300

    create new solution

--- Core/how-to-NPM/03:Start-A-Project.js
+++ Core/how-to-NPM/03:Start-A-Project.js
@@ -1,7 +1,7 @@
-/* Challenge #3 Login
+/* Challenge #3 Start A Project
 Code by Perrin Clark
 BlackGuyCoding@gmail.com
 https://www.nodeschool.io */
 
 
-//this challenge requires you to create a new npm user//
+//this challenge requires you to start a new project by creating a package.json file. For additional help, follow this tutorial: https://nodesource.com/blog/your-first-nodejs-package/ //
