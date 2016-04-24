63f8d0cc9a262a2991aa20f027bcc3111111d221
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 20:02:52 +0300

    create new solutions

---
+++ Core/how-to-NPM/00:Install-npm.js
@@ -0,0 +1,7 @@
+/* Challenge #1 Install npm
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.nodeschool.io */
+
+
+//this challenge requires you to install or verify your current version of npm//
