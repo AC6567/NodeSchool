f73d8ed687b8eeb1b2b367512f123052d10e000d
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 20:02:55 +0300

    create new solutions

---
+++ Core/how-to-NPM/01:Dev-Environment.js
@@ -0,0 +1,7 @@
+/* Challenge #2 Dev Environment
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.nodeschool.io */
+
+
+//this challenge requires you to create a new directory and 'cd' or 'change directory' into it//
