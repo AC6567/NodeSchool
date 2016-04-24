e239a3efa7f519eae2df74f0b92eee5418c008ce
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 18:15:44 +0300

    create new solution

---
+++ Core/git-it/10:requesting-you-pull-please.js
@@ -0,0 +1,7 @@
+/* Challenge #7 branches aren't just for birds
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.nodeschool.io */
+
+
+//this challenge requires you to submit a pull request on the original patchwork repository//
