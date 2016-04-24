65c0618acdec9f91df56b17b1c75830b4d97526e
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 18:15:47 +0300

    create new solution

---
+++ Core/git-it/11:merge-tada.js
@@ -0,0 +1,7 @@
+/* Challenge #7 branches aren't just for birds
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.nodeschool.io */
+
+
+//this challenge requires you to merge your branch locally, delete old branch and pull from upstream for much win!//
