78a797787ef7d8cfa3eb77f6f553f65acbefba1a
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 21:20:38 +0300

    create new solution

---
+++ Core/how-to-NPM/05:Listing-Dependencies.js
@@ -0,0 +1,7 @@
+/* Challenge #5 Listing Dependencies
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.nodeschool.io */
+
+
+//this challenge requires you to show a list of your dependencies that were just created //
