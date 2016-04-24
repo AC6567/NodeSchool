d8115d077e44a1861a1e47f1f824f4478689bdf1
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 16:33:04 +0300

    create new nodeschool solution

---
+++ Core/javascripting/17:functions.js
@@ -0,0 +1,10 @@
+/* Challenge #17: functions
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.nodeschool.io */
+
+function eat (food) {
+  return food + ' tasted really good.';
+}
+
+console.log(eat('bananas'));
