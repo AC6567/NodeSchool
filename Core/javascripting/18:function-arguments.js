17579ab6b4406f179bd3feafcef9ee7e222a4939
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 16:33:06 +0300

    create new nodeschool solution

---
+++ Core/javascripting/18:function-arguments.js
@@ -0,0 +1,10 @@
+/* Challenge #18: function arguments
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.nodeschool.io */
+
+function math(a, b, c) {
+  return (b * c) + a;
+}
+
+console.log(math(53, 61, 67));
