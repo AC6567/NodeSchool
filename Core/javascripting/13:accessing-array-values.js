83cd1c450876aa85ad4bef3c08edb0c8eb4f6814
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 16:32:51 +0300

    create new nodeschool solution

---
+++ Core/javascripting/13:accessing-array-values.js
@@ -0,0 +1,8 @@
+/* Challenge #13: accessing array value
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.nodeschool.io */
+
+var food = ['apple', 'pizza', 'pear'];
+
+console.log(food[1]);
