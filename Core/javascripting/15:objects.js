8290fbbd18953d6b1d9fa46de7bfbe47fef0c6f4
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 16:32:58 +0300

    create new nodeschool solution

---
+++ Core/javascripting/15:objects.js
@@ -0,0 +1,12 @@
+/* Challenge #15: objects
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.nodeschool.io */
+
+var pizza = {
+  toppings: ['cheese', 'sauce', 'pepperoni'],
+  crust: 'deep dish',
+  serves: 2,
+};
+
+console.log(pizza);
