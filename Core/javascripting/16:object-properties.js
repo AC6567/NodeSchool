cfa3ebd571200ec027ef8bb8dd19df133e8e6306
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 16:33:01 +0300

    create new nodeschool solution

---
+++ Core/javascripting/16:object-properties.js
@@ -0,0 +1,10 @@
+/* Challenge #16: object properties
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.nodeschool.io */
+
+var food = {
+  types: 'only pizza'
+};
+
+console.log(food.types);
