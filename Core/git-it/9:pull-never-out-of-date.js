2a83af36930baa8811605cf5e2fba67324eec360
Perrin Clark <blackguycoding@gmail.com>
2016-04-24 18:15:41 +0300

    create new solution

---
+++ Core/git-it/9:pull-never-out-of-date.js
@@ -0,0 +1,7 @@
+/* Challenge #7 branches aren't just for birds
+Code by Perrin Clark
+BlackGuyCoding@gmail.com
+https://www.nodeschool.io */
+
+
+//this challenge requires you to keep your file up to date by pulling in changes from collaborators//
